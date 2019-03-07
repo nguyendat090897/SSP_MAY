66fae43c5701560c6aecd80f9ac13bc1139115f5
tran minh <leminh.tran.1997@gmail.com>
2019-03-07 21:19:23 +0700

    Update

--- WAREHOUSE_SPP/4.Souce Code/Application/DSofT.Warehouse.Presentation/DSofT.Warehouse.UI/frm_NhapKho_popup.xaml.cs
+++ WAREHOUSE_SPP/4.Souce Code/Application/DSofT.Warehouse.Presentation/DSofT.Warehouse.UI/frm_NhapKho_popup.xaml.cs
@@ -1275,15 +1275,15 @@ namespace DSofT.Warehouse.UI
             try
             {
                 object xReturn = null;
-                xReturn = UIAgent.GetUIPopupDialog(this, null, this.GetType().ToString(), "DSofT.Warehouse.UI", "frm_XDDinhMuc_BoSung_TTTViet_TimKiemSP", null);
-                //if (pN_OR_N == "N")
-                //{
-                //    xReturn =  UIAgent.GetUIPopupDialog(this, null, this.GetType().ToString(), "DSofT.Warehouse.UI", "frm_XDDinhMuc_BoSung_TTTViet_TimKiemSP", null);
-                //}
-                //else
-                //{
-                //    xReturn = UIAgent.GetUIPopupDialog(this, null, this.GetType().ToString(), "DSofT.Warehouse.UI", "frm_XDDinhMuc_BoSung_TTTViet_TimKiemSP_Kho", null);
-                //}
+                //xReturn = UIAgent.GetUIPopupDialog(this, null, this.GetType().ToString(), "DSofT.Warehouse.UI", "frm_XDDinhMuc_BoSung_TTTViet_TimKiemSP", null);
+                if (pN_OR_N == "N")
+                {
+                    xReturn = UIAgent.GetUIPopupDialog(this, null, this.GetType().ToString(), "DSofT.Warehouse.UI", "frm_XDDinhMuc_BoSung_TTTViet_TimKiemSP", null);
+                }
+                else
+                {
+                    xReturn = UIAgent.GetUIPopupDialog(this, null, this.GetType().ToString(), "DSofT.Warehouse.UI", "frm_XDDinhMuc_BoSung_TTTViet_TimKiemSP_Kho", null);
+                }
                 DataTable dt_SPCHON = (DataTable)xReturn;
                 if ((dt_SPCHON != null) && (dt_SPCHON.Rows.Count > 0))
                 {
@@ -1310,10 +1310,10 @@ namespace DSofT.Warehouse.UI
                                 {
                                     //dr["SOLO"] = dt_SPCHON.Rows[i]["SOLO"];
                                     //dr["KHO_KHUVUC_ID"] = dt_SPCHON.Rows[i]["KHO_KHUVUC_ID"];
-                                    grd.Columns["KHO_KHUVUC_ID"].Visible = false;
+                                    //grd.Columns["KHO_KHUVUC_ID"].Visible = false;
                                     dr["KHO_ID"] = dt_SPCHON.Rows[i]["KHO_ID"];
                                     dr["HANDUNG"] = dt_SPCHON.Rows[i]["HANDUNG"];
-                                    dr["SO_LUONG_TON"] = dt_SPCHON.Rows[i]["SO_LUONG_TON"];
+                                    //dr["SO_LUONG_TON"] = dt_SPCHON.Rows[i]["SO_LUONG_TON"];
                                     dr["VITRI"] = dt_SPCHON.Rows[i]["VITRI"];
                                 }
                                 else
